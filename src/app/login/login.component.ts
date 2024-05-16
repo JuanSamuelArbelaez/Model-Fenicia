070492de933d775edf897988b2e95d463af2bf5b
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2024-05-15 23:23:26 -0500

    Register and Login Forms and styles implemented.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators  } from '@angular/forms';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, ]],
+    });
+  }
+
+  onSubmit(): void {
+    if (!this.loginForm.valid){
+      alert('Error')
+    }
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent, ReactiveFormsModule, CommonModule,],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      nickname: ['', Validators.required],
+      password: ['', Validators.required, Validators.minLength(8)],
+      passwordConfirm: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (!this.registerForm.valid){
+      alert('Error')
+    }
+  }
+
+  isPasswordMatch(): boolean {
+    const password = this.registerForm.get('password')?.value;
+    const confirmPassword = this.registerForm.get('passwordConfirm')?.value;
+    return password === confirmPassword;
+  }
 }
